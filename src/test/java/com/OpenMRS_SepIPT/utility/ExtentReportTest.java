d41b90fc3708f7d70cd5cb0a7905f14698ee8846
satishnaughty <sathishsathish1429@gmail.com>
2024-09-23 11:39:27 +0530

    commit the code

--- src/test/java/com/OpenMRS_SepIPT/utility/ExtentReportTest.java
+++ src/test/java/com/OpenMRS_SepIPT/utility/ExtentReportTest.java
@@ -1,5 +1,29 @@
 package com.OpenMRS_SepIPT.utility;
 
+import java.io.IOException;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.base_class_omrs.Base_class;
+
 public class ExtentReportTest {
+	
+public static ExtentTest extenttest;
+	
+	@BeforeSuite
+	public void extentReortstartup() {
+    Base_class base = new Base_class();
+      base.extentReportStart(null);
+	}
+	
+	@AfterSuite
+	public void extentreport() throws IOException {
+      Base_class base = new Base_class();
+       base.extentReportsTearDown(null);
+	}
+	
+
 
 }
